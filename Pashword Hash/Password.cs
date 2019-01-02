a5de6f2d4770d70501df464b02cd745cc57939e7
Erik Madsen <ekmadsen@comcast.net>
2019-01-02 16:51:13 -0600

    Added Password Hash project.

---
+++ Pashword Hash/Password.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace ErikTheCoder.Sandbox.PasswordHash
+{
+    public static class Password
+    {
+        public static (string Salt, string Hash) Hash(string Password, int SaltLength, int HashLength, int Iterations)
+        {
+            // Create random salt.
+            // Storing a salt value with a hashed password prevents identical passwords from hashing to the same stored value.
+            // See https://security.stackexchange.com/questions/17421/how-to-store-salt
+            byte[] saltBytes = new byte[SaltLength];
+            using (RNGCryptoServiceProvider random = new RNGCryptoServiceProvider())
+            {
+                random.GetBytes(saltBytes, 0, SaltLength);
+                string salt = Convert.ToBase64String(saltBytes);
+                // Get derived bytes from the combined salt and password, using the specified number of iterations.
+                using (Rfc2898DeriveBytes derivedBytes = new Rfc2898DeriveBytes(Password, saltBytes, Iterations))
+                {
+                    byte[] hashBytes = derivedBytes.GetBytes(HashLength);
+                    string hash = Convert.ToBase64String(hashBytes);
+                    return (salt, hash);
+                }
+            }
+        }
+
+
+        public static bool Validate(string Password, string Salt, string Hash, int Iterations)
+        {
+            byte[] saltBytes = Convert.FromBase64String(Salt);
+            int hashLength = Convert.FromBase64String(Hash).Length;
+            using (Rfc2898DeriveBytes derivedBytes = new Rfc2898DeriveBytes(Password, saltBytes, Iterations))
+            {
+                byte[] hashBytes = derivedBytes.GetBytes(hashLength);
+                string hash = Convert.ToBase64String(hashBytes);
+                return hash == Hash;
+            }
+        }
+    }
+}
---
+++ Pashword Hash/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace ErikTheCoder.Sandbox.PasswordHash
+{
+    public static class Program
+    {
+        public static void Main(string[] Args)
+        {
+            const int saltLength = 16;
+            const int hashLength = 32;
+            const int iterations = 1000;
+            const string password = "OpenSeysMe";
+            (string salt, string hash) = Password.Hash(password, saltLength, hashLength, iterations);
+            Console.WriteLine($"Salt = {salt}.");
+            Console.WriteLine($"Hash = {hash}.");
+            Console.WriteLine($"Password valid = {Password.Validate(password, "3RYC5Ex6UCokBTRwawJ5tw==", "xFtit2Q447+E2hR7ilmAlWIFjOINgqgrg0pesQ0Vyd0=", iterations)}.");
+            Console.ReadLine();
+        }
+    }
+}
